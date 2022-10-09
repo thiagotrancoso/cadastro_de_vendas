a48f9a30c5449c83df7010282e0796af3d7d14b2
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-09 09:47:25 -0300

    Adicionado pacote "admin-lte"

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,34 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+  //----------------------------------------------------------------------
+  // App
+  //----------------------------------------------------------------------
+  // AdminLte - CSS
+  .copy('./node_modules/admin-lte/dist/css/adminlte.min.css', './public/assets/app/css/adminlte.min.css')
+  .copy('./node_modules/admin-lte/dist/css/adminlte.min.css.map', './public/assets/app/css/adminlte.min.css.map')
+
+  // AdminLte - JS
+  .copy('./node_modules/admin-lte/dist/js/adminlte.min.js', './public/assets/app/js/adminlte.min.js')
+  .copy('./node_modules/admin-lte/dist/js/adminlte.min.js.map', './public/assets/app/js/adminlte.min.js.map')
+
+  // AdminLte - Plugin: jquery
+  .copy('./node_modules/admin-lte/plugins/jquery/jquery.min.js', './public/assets/app/plugins/jquery/jquery.min.js')
+  .copy('./node_modules/admin-lte/plugins/jquery/jquery.min.map', './public/assets/app/plugins/jquery/jquery.min.map')
+
+  // AdminLte - Plugin: bootstrap
+  .copy('./node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js', './public/assets/app/plugins/bootstrap/js/bootstrap.bundle.min.js')
+  .copy('./node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js.map', './public/assets/app/plugins/bootstrap/js/bootstrap.bundle.min.js.map')
+
+  // AdminLte - Plugin: Fontawesome
+  .copyDirectory('./node_modules/admin-lte/plugins/fontawesome-free', './public/assets/app/plugins/fontawesome-free')
+
+  // AdminLte - Plugin: overlayScrollbars
+  .copyDirectory('./node_modules/admin-lte/plugins/overlayScrollbars', './public/assets/app/plugins/overlayScrollbars')
+
+  // AdminLte - Plugin: SweetAlert2
+  .copyDirectory('./node_modules/admin-lte/plugins/sweetalert2', './public/assets/app/plugins/sweetalert2')
+
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css');
