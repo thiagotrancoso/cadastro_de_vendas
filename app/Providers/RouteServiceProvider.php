14ceae7f146417c948fb628cb499bac34c0a5aba
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-10 20:13:14 -0300

    Alterado redirect de "home" para "app"

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,6 +23,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    public const APP = '/app';
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
