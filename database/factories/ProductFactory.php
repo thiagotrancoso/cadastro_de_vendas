b2718c69e3b24e77fe00e6de00f7e12829798968
Thiago Trancoso <thiagostrn@gmail.com>
2022-10-12 11:54:13 -0300

    Criado estrutura de banco de dados Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(2),
+        'price' => $faker->randomFloat(2, 10, 100),
+        'description' => $faker->text(300),
+        'active' => $faker->randomElement([true, false]),
+        'user_id' => 1,
+        'updated_at' => null
+    ];
+});
